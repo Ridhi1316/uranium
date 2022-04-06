150f25770a812f2c445eda8e14d6847ce5202878
“Ridhi1316” <ridhi9697@gmail.com>
2022-04-06 19:37:36 +0530

    trying to do

---
+++ src/logger.js
@@ -0,0 +1,5 @@
+let endpoint = 'https://www.google.com'
+let log=function() {
+    console.log('I m inside log functio')
+}
+module.exports.endpoint=endpoint
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
+const logger = require('./logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    console.log('The endpoint is :',logger.endpoint)
+    
     res.send('My first ever api!')
+
+});
+
+router.get('/test-me2', function (req, res) {
+    res.send('My second ever api!')
 });
 
 module.exports = router;
